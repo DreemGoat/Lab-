f6325cf2fa5921badf3cc7304a73a27039df7763
DreemGoat <91465902+DreemGoat@users.noreply.github.com>
2021-10-08 16:22:02 +0700

    Delete exercise 2 directory

--- exercise 2/Perimeter of a Triangle.py
+++
@@ -1,8 +0,0 @@
-e1 = float(input("Enter length of edge1: "))
-e2 = float(input("Enter length of edge2: "))
-e3 = float(input("Enter length of edge3: "))
-if e1+e2>e3 and e2<e1+e3 and e2+e3>e1:
-    p = eval("e1+e2+e3")
-    print("The perimeter is", p)
-else:
-    print("Perimeter cannot be calculated: the input is invalid")
--- exercise 2/Ramp Angle.py
+++
@@ -1,7 +0,0 @@
-import math
-g =9.8
-m = float(input("Enter the mass of the cart(in kg): "))
-F = float(input("Enter the force to push the cart (in N): "))
-t = math.sinh(F/(m*g))
-t = math.degrees(t)
-print("The angle of the ramp is", round(t,1), "degrees")
