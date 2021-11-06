a0d93f93cf2ac52e8870d03f58f602099b2a3678
DreemGoat <91465902+DreemGoat@users.noreply.github.com>
2021-11-06 20:49:58 +0700

    Week 6 Edit

---
+++ Lab work/Week 6/Favorite Car Models.py
@@ -0,0 +1,8 @@
+MyDict ={"Zeus":("Equator","Alphard","Lexus"),"Steph":("Ativa", "Mini Cooper", "Lambourghini"),"Wilbert":("Golf","Bezza","Proton"),"Filbert":("Axia","Mustang","Ferrari"),"Chara":("Jungle Jeep","Honda Civic","Leyland")}
+for x in MyDict:
+    print("The favorite car model of",x,"is")
+    print("-",(MyDict[x])[0])
+    print("-",(MyDict[x])[1])
+    print("-",(MyDict[x])[2])
+    print("")
+print("AAAAAH I NEED A MEDIC BAG")
---
+++ Lab work/Week 6/Programming Language dictionary.py
@@ -0,0 +1,7 @@
+MyDict = {"Python":"Beginner Friendly language","C++":"an extension for C language","Java":"high-level, class-based, object-oriented programming language","C#":"general-purpose, multi-paradigm programming language.","C":"general-purpose, procedural computer programming language supporting structured programming, lexical variable scope, and recursion, with a static type system."}
+Order = {1:"Python",2:"C++",3:"Java",4:"C#",5:"C"} #order of the dictionary
+print(Order[3],":",MyDict[Order[3]])
+print(Order[2],":",MyDict[Order[2]])
+print(Order[4],":",MyDict[Order[4]])
+print(Order[1],":",MyDict[Order[1]])
+print(Order[5],":",MyDict[Order[5]])
